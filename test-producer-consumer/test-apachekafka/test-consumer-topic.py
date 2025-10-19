439a83bafd81d83a5c4e852125785f7cb100bb65
Md. Ruhul Amin <ruhul@ba-systems.com>
2025-10-19 18:23:59 +0600

    Making apache-kafka-4.1.0 runnable

---
+++ test-producer-consumer/test-apachekafka/test-consumer-topic.py
@@ -0,0 +1,17 @@
+from kafka import KafkaConsumer
+import json
+
+# Create Kafka consumer
+consumer = KafkaConsumer(
+    'quickstart-events',
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='my-consumer-group-w',
+    value_deserializer=lambda v: json.loads(v.decode('utf-8'))
+)
+
+print("Listening to messages from topic: quickstart-events")
+
+for message in consumer:
+    print(f"Received message: {message.value}")
---
+++ test-producer-consumer/test-apachekafka/test-producer-topic.py
@@ -0,0 +1,22 @@
+from kafka import KafkaProducer
+import json
+import time
+
+# Create Kafka producer
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],  # Your Kafka broker
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+topic = "quickstart-events"
+
+print("Producing messages to Kafka topic:", topic)
+
+for i in range(10):
+    message = {"id": i, "message": f"Hello Kafka {i}"}
+    producer.send(topic, message)
+    print(f"Sent: {message}")
+    time.sleep(1)  # small delay for readability
+
+producer.flush()
+print("✅ All messages sent successfully!")
